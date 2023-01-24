c7de66273dc0ba77671f6e03a01cae74796799e3
Pheon-Dev <devpheon@gmail.com>
2023-01-24 15:37:30 +0300

    feat: rust aliases

--- alias.zsh
+++ alias.zsh
@@ -1,17 +1,21 @@
 # ~/.zshrc
+
+# EXA
 alias ls="exa --icons --color=always --group-directories-first"
 alias la="exa --icons -abghHliS --color=always --group-directories-first"
 alias l="exa --icons -la --color=always --group-directories-first"
 alias ll="exa --icons -a --color=always --group-directories-first"
 alias lt="exa --icons -T --color=always --group-directories-first"
 alias l.='exa -a | egrep "^\."'
-alias lv="$HOME/.local/bin/lvim"
+
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep='fgrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 alias x="exit"
 alias ytl="youtube-dl"
 alias shuffle="mpv --shuffle"
+
+# MOCP
 alias mus="mocp"
 alias msh="mocp -t shuffle"
 alias mnx="mocp -t autonext"
@@ -23,29 +27,38 @@ alias mov="mocp -v"
 alias moi="mocp -i"
 alias mos='mocp -i | grep -E "File" | cut -d "/" -f 6'
 alias vol="pulsemixer"
-alias pmu="cd $HOME/Music"
-alias dw="cd $HOME/Downloads/"
-alias play="cd $HOME/Documents/Play-Ground/"
-alias dwm="cd $HOME/Downloads/Movies/"
-alias dc="cd $HOME/Documents/"
+
+alias dow="cd $HOME/Downloads/"
+alias doc="cd $HOME/Documents/"
+
 alias xf="exiftool"
+
 alias nvg="wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash"
 alias nvl="nvm install node" #latest
 alias nvi="nvm install" #nvi 10.18.1
+
 alias apnx="pnpx create-next-app"
 alias apre="pnpx create-react-app"
 alias apvi="pnpm create vite || npm create vite"
 alias apsv="pnpx degit sveltejs/template"
+
 alias pcon="p10k configure"
+
+# TMUX
 alias t="tmux"
 alias tn="tmux new-session -s"
 alias ta="tmux attach"
 alias tk="tmux kill-server"
 alias ts="tmux source $HOME/.tmux.conf"
+
+# EDITOR
 alias v="$EDITOR"
 alias vi="$EDITOR"
 alias vim="$EDITOR"
+
 alias cat="bat -pp"
+
+# NPNPM
 alias npd="pnpm run dev"
 alias npw="pnpm run watch"
 alias nps="pnpm run start"
@@ -61,13 +74,20 @@ alias npau="npm audit fix"
 alias pnau="pnpm audit fix"
 alias npaf="npm audit fix --force"
 alias pnaf="pnpm audit fix --force"
+
 alias snapi="snap install"
+
+# XANDR
 # alias disn="xrandr --output eDP-1 --off && killall conky"
 # alias disf="xrandr --output eDP-1 --auto" # HDMI-1
 alias dish="xrandr --output eDP-1 --off --output HDMI-1 --auto; nitrogen --restore"
 alias dise="xrandr --output HDMI-1 --off --output eDP-1 --auto; nitrogen --restore"
 alias disa='xrandr --output eDP-1 --primary --auto --right-of HDMI-1 --auto; nitrogen --restore'
+
 alias sss='systemctl suspend'
+alias e='archlinux-logout'
+
+# CONFIG
 alias afg="cd $HOME/.config/alacritty && $EDITOR"
 alias dfg="cd $HOME/.config/arco-dwm && $EDITOR"
 alias kfg="cd $HOME/.config/kitty && $EDITOR"
@@ -80,6 +100,8 @@ alias nfg="cd $HOME/.config/nvim && $EDITOR"
 alias zfg="cd $HOME/.config/zsh && $EDITOR"
 alias jfg="cd $HOME/.config/zellij && $EDITOR"
 alias bfg="cd $HOME/.config/dwmbar && $EDITOR"
+
+# BLUETOOTH
 alias blf='bluetooth off'
 alias bln='bluetooth on'
 alias bls="bluetoothctl scan on"
@@ -88,15 +110,21 @@ alias blr="bluetoothctl connect"
 alias bli="bluetoothctl info"
 alias bld="bluetoothctl disconnect CC:98:8B:A7:BB:38"
 alias blp="bluetoothctl pair CC:98:8B:A7:BB:38"
+
 alias mntl="sudo lsblk -l" # fdisk
-alias pacu="sudo pacman -Syyu"
-alias pacl="sudo rm /var/lib/pacman/db.lck"
+
+# YAY
 alias yaya="yay -Sua --noconfirm"
 alias yayu="yay -Syyu"
 alias yays="yay -Syu --noconfirm"
+
+# PACMAN
+alias pacu="sudo pacman -Syyu"
+alias pacl="sudo rm /var/lib/pacman/db.lck"
 alias paci="sudo pacman -S"
 alias pacu="sudo pacman -Syyu"
 alias pacs="sudo pacman -Syu --noconfirm"
+
 alias alis="alias | grep"
 alias icat="kitty +kitten icat --align=left"
 alias lis="live-server"
@@ -105,13 +133,19 @@ alias c='clear'
 alias rn='ranger'
 alias ari='aria2c --file-allocation=none -c -x 10 -d . -Z'
 alias tp='toipe'
+
 alias pnu='sudo pnpm add -g pnpm'
 alias npu='sudo npm update --location=global'
+
 alias lg='lazygit'
+
 alias pnb='pnpm run build'
 alias pnt='pnpm create tauri-app'
+
 alias deni='deno run -A -r https://fresh.deno.dev'
 alias dens='deno task start'
+
+# YARN
 alias y="yarn"
 alias ya="yarn add"
 alias yr="yarn remove"
@@ -124,24 +158,31 @@ alias ys="yarn start"
 alias yi="yarn init"
 alias yx="yarn create next-app --typescript" # alt [ynx app-name]
 alias ye="yarn add --ignore-engines"
+
 alias f="fuck"
 alias fsg="flameshot gui -p ~/Downloads/Picture"
 alias bl="bluetooth on && bluetoothctl connect CC:98:8B:A7:BB:38"
 alias psc="pscale"
+
+# PRISMA
 alias pft="prisma format"
 alias psh="prisma db push"
 alias pll="prisma pull"
 alias pnt="prisma init"
 alias pgn="prisma generate"
 alias pst="prisma studio"
+
 alias mcon="cd $HOME/.config/dmenu && mv config.h bu-config.h && sudo make install"
 alias dcon="cd $HOME/.config/arco-dwm && mv config.h bu-config.h && sudo make install"
 alias scon="cd $HOME/.config/arco-st && mv config.h bu-config.h && sudo make install"
+
 alias nv="~/Documents/Neovim/nvim-linux64/bin/nvim"
 alias rb="reboot"
 alias fsh="shfmt -l -w ."
 alias sls="slides"
 alias sl="slock"
+
+# GO
 alias gobi="go build . && go install"
 alias goi="go install"
 alias gob="go build"
@@ -150,9 +191,18 @@ alias gon="go run ."
 alias goy="go mod tidy"
 alias f="flamingo && clear"
 alias pk="peek"
+
+# ZELLIJ
 alias zz="zellij"
 alias za="zellij attach"
 alias zs="zellij list-sessions"
 alias zn="zellij --session"
 alias zd="zl defo"
 alias zf="zellij edit --floating --"
+alias h="helix"
+
+# CARGO
+alias cn="cargo new"
+alias ca="cargo add"
+alias cr="cargo run"
+alias cb="cargo build"
