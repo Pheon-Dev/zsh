54533ed83453a353ee80e9b7cb1a2dfb01be6153
Pheon-Dev <devpheon@gmail.com>
2023-02-24 22:06:51 +0300

    chore: components (_gt_help, _gt_repo) and Gitlab Function

--- functions.sh
+++ functions.sh
@@ -220,47 +220,63 @@ got () {
     echo " "
 }
 
-gtl () {
+_gt_help () {
+  echo "$(tput setaf 1) ** Missing Flag Option ** \n"
+  echo -n "$(tput setaf 2) gt"
+  echo -n "$(tput setaf 3) <option>"
+  echo "$(tput setaf 4) <repo> \n"
+  echo -e "$(tput setaf 5) List of flag options :"
+  echo -n "$(tput setaf 3)     --set, -s  "
+  echo -n "$(tput setaf 8) →"
+  echo -e "$(tput setaf 6) Set Remote URL for Origin"
+  echo -n "$(tput setaf 3)     --init, -i "
+  echo -n "$(tput setaf 8) →"
+  echo -e "$(tput setaf 6) Set Remote URL for Origin \n"
+
+}
+
+_gt_repo () {
+  echo "$(tput setaf 1) ** Missing Repo Name ** \n"
+  echo -n "$(tput setaf 2) gt"
+  echo -n "$(tput setaf 3) <option>"
+  echo -n "$(tput setaf 4) <repo> \n\n"
+  echo -e "$(tput setaf 5) Repository name :"
+  echo -n "$(tput setaf 4)     <repo-name> "
+  echo -n "$(tput setaf 8) →"
+  echo -e "$(tput setaf 6) Custom Repository Name \n "
+
+}
+
+glb () {
     echo " "
-    if [[ $2 == "" ]]; then
-        if [[ $1 == "" ]]; then
-            echo "$(tput setaf 1) ** Missing Flag Option ** \n"
-            echo -n "$(tput setaf 2) gt"
-            echo -n "$(tput setaf 3) <option>"
-            echo "$(tput setaf 4) <repo> \n"
-            echo -e "$(tput setaf 5) List of flag options :"
-            echo -n "$(tput setaf 3)     --set, --s  "
-            echo -n "$(tput setaf 8) →"
-            echo -e "$(tput setaf 6) Set Remote URL for Origin"
-            echo -n "$(tput setaf 3)     --init, --i "
-            echo -n "$(tput setaf 8) →"
-            echo -e "$(tput setaf 6) Set Remote URL for Origin \n"
-            return 1
-        fi
-        echo "$(tput setaf 1) ** Missing Repo Name ** \n"
-        echo -n "$(tput setaf 2) gt"
-        echo -n "$(tput setaf 3) <option>"
-        echo -n "$(tput setaf 4) <repo> \n"
-        echo -e "$(tput setaf 5) Repository name :"
-        echo -n "$(tput setaf 4)     <repo-name> "
-        echo -n "$(tput setaf 8) →"
-        echo -e "$(tput setaf 6) Custom Repository Name \n "
+    if [[ $1 == "" ]]; then
+      _gt_help
         return 1
     fi
 
+    repo_name=""
     if [[ $1 == "--set" || $1 == "-s" ]]; then
-            echo -n "$(tput setaf 3)  GitLab"
-            echo -e "$(tput setaf 6) Remote URL Successfully Set : \n"
-            git remote set-url origin https://oauth2:$GITLAB_TOKEN@gitlab.com/devpheon/$2.git
+      repo_name=$(git remote -v | grep -E "fetch" | cut -d " " -f 1 | awk 'BEGIN { FS = " " } { print $2 }' | awk 'BEGIN { FS = "/" } { print $5 }' | awk 'BEGIN { FS = "." } { print $1 }')
+      echo -n "$(tput setaf 3)  GitLab"
+      echo -e "$(tput setaf 6) Remote URL Successfully Set : \n"
+      git remote set-url origin https://oauth2:$GITLAB_TOKEN@gitlab.com/devpheon/$2.git
     fi
+
     if [[ $1 == "--init" || $1 == "-i" ]]; then
-        echo -n "$(tput setaf 3)  GitLab"
-        echo -e "$(tput setaf 6) Repository Successfully Initialised : \n"
-        git init > /dev/null 2>&1 && git remote add origin https://githlab.com/devpheon/$2.git && git branch -M main && git remote set-url origin https://oauth2:$GITLAB_TOKEN@gitlab.com/devpheon/$2.git && touch .gitignore README.md
+      if [[ $2 == "" ]]; then
+        _gt_repo
+          return 1
+      fi
+
+      repo_name=$2
+      echo -n "$(tput setaf 3)  GitLab"
+      echo -e "$(tput setaf 6) Repository Successfully Initialised : \n"
+      git init > /dev/null 2>&1 && git remote add origin https://githlab.com/devpheon/$repo_name.git && git branch -M main && git remote set-url origin https://oauth2:$GITLAB_TOKEN@gitlab.com/devpheon/$repo_name.git && touch .gitignore README.md
     fi
+
     echo -n "$(tput setaf 2)      "
     echo -n "$(tput setaf 8) →"
-    echo -e "$(tput setaf 4) $2"
+    echo -e "$(tput setaf 4) $repo_name"
     echo -n "$(tput setaf 2)      "
     echo -n "$(tput setaf 8) →"
     echo -n "$(tput setaf 5) "
@@ -271,18 +287,7 @@ gtl () {
 gt () {
     echo " "
     if [[ $1 == "" ]]; then
-        echo "$(tput setaf 1) ** Missing Flag Option ** \n"
-        echo -n "$(tput setaf 2) gt"
-        echo -n "$(tput setaf 3) <option>"
-        echo "$(tput setaf 4) <repo> \n"
-        echo -e "$(tput setaf 5) List of flag options :"
-        echo -n "$(tput setaf 3)     --set, --s  "
-        echo -n "$(tput setaf 8) →"
-        echo -e "$(tput setaf 6) Set Remote URL for Origin"
-        echo -n "$(tput setaf 3)     --init, --i "
-        echo -n "$(tput setaf 8) →"
-        echo -e "$(tput setaf 6) Set Remote URL for Origin \n"
-
+      _gt_help
         return 1
     fi
 
@@ -293,18 +298,13 @@ gt () {
       echo -e "$(tput setaf 6) Remote URL Successfully Set : \n"
       git remote set-url origin https://$GITHUB_TOKEN@github.com/Pheon-Dev/$repo_name.git
     fi
+
     if [[ $1 == "--init" || $1 == "-i" ]]; then
       if [[ $2 == "" ]]; then
-          echo "$(tput setaf 1) ** Missing Repo Name ** \n"
-          echo -n "$(tput setaf 2) gt"
-          echo -n "$(tput setaf 3) <option>"
-          echo -n "$(tput setaf 4) <repo> \n\n"
-          echo -e "$(tput setaf 5) Repository name :"
-          echo -n "$(tput setaf 4)     <repo-name> "
-          echo -n "$(tput setaf 8) →"
-          echo -e "$(tput setaf 6) Custom Repository Name \n "
+        _gt_repo
           return 1
       fi
+
       repo_name=$2
       echo -n "$(tput setaf 3)  GitHub"
       echo -e "$(tput setaf 6) Repository Successfully Initialised : \n"
