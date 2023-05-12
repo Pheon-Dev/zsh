81661302bb20246b3fa9103d2800b448cf149c30
Pheon-Dev <devpheon@gmail.com>
2023-05-12 12:28:07 +0300

    feat: mpv23 - mkv to mp3 converter command

--- functions/functions.zsh
+++ functions/functions.zsh
@@ -1,4 +1,8 @@
 #!/bin/bash zsh
+#
+mpv23 () {
+  find . -type f -name "*.mkv" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -c:a libmp3lame -y "${FILE%.mkv}.mp3";rm -rf $FILE' _ '{}' \;
+}
 
 ports () {
   help () {
