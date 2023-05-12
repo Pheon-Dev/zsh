bf2856043a83a8474d0267362b981dcb2878d059
Pheon-Dev <devpheon@gmail.com>
2023-05-12 12:31:33 +0300

    chore: rename command

--- functions/functions.zsh
+++ functions/functions.zsh
@@ -1,6 +1,6 @@
 #!/bin/bash zsh
 #
-mpv23 () {
+v23 () {
   find . -type f -name "*.mkv" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -c:a libmp3lame -y "${FILE%.mkv}.mp3";rm -rf $FILE' _ '{}' \;
 }
 
