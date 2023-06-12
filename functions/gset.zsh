9f6c7b34ca949af38e763246d198f96b55ece1da
Pheon-Dev <devpheon@gmail.com>
2023-06-12 23:28:23 +0300

    [+] rust projects

--- functions/gset.zsh
+++ functions/gset.zsh
@@ -15,11 +15,18 @@ gset () {
   
   cd $HOME/.tmux && gt -s
   cd $HOME/.nb/home && gt -s
+
   cd $HOME/Documents/Rust/m-pesa && gt -s
   cd $HOME/Documents/Rust/manta && gt -s
+  cd $HOME/Documents/Rust/manta-api && gt -s
+  cd $HOME/Documents/Rust/manta-wallet && gt -s
+
   cd $HOME/Documents/Python/kophee && gt -s
+
   cd $HOME/Documents/go/src/github.com/Pheon-Dev/flamingo && gt -s
+
   cd $HOME/Documents/Neovim/antelope && gt -s
+  cd $HOME/Documents/Neovim/pigeon && gt -s
   cd $HOME/Documents/Neovim/lir.nvim && gt -s
   cd $HOME/Documents/Neovim/harpoon && gt -s
   cd $HOME/Documents/Neovim/lsp_lines.nvim && gt -s
