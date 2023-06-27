6e7f37fafd6459714da00d62ee10c9f4dbbc36c2
Pheon-Dev <devpheon@gmail.com>
2023-06-27 23:01:36 +0300

    [+] border colour

--- fzf.zsh
+++ fzf.zsh
@@ -1,3 +1,3 @@
 #!/bin/bash zsh
-
-export FZF_DEFAULT_OPTS='--color=fg:#c0caf5,bg:#21222c,hl:#bd93f9 --color=fg+:#c0caf5,bg+:#21222c,hl+:#bd93f9 --color=info:#f1fa8c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#f1fa8c,header:#6272a4'
+#282a36
+export FZF_DEFAULT_OPTS='--color=fg:#c0caf5,bg:#21222c,hl:#bd93f9,border:#44475a --color=fg+:#c0caf5,bg+:#21222c,hl+:#bd93f9 --color=info:#f1fa8c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#f1fa8c,header:#6272a4'
