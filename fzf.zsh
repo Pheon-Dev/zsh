c0b39ded47355d6e04886cc6c1d9384341701427
Pheon-Dev <devpheon@gmail.com>
2023-06-29 14:08:05 +0300

    [-] tab and source

--- fzf.zsh
+++ fzf.zsh
@@ -1,7 +1,5 @@
 #!/bin/bash zsh
 # 282a36 https://github.com/junegunn/fzf/blob/master/ADVANCED.md
-# source "$HOME/.config/zsh/fzf-tab/fzf-tab.zsh"
-# source "$HOME/.config/zsh/fzf-tab-source/fzf-tab-source.plugin.zsh"
 
 export FZF_DEFAULT_OPTS='--color=fg:#c0caf5,bg:#21222c,hl:#bd93f9,border:#44475a --color=fg+:#c0caf5,bg+:#21222c,hl+:#bd93f9 --color=info:#f1fa8c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#f1fa8c,header:#6272a4'
 
