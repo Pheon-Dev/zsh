9a0f7a35d6a58d069c6480674453162e567d1f02
Pheon-Dev <devpheon@gmail.com>
2023-06-29 21:15:51 +0300

    [~] order

--- zsh-plugins/zsh-source-plugins.zsh
+++ zsh-plugins/zsh-source-plugins.zsh
@@ -4,5 +4,5 @@ source "$HOME/.config/zsh/zsh-plugins/zsh-syntax-highlighting/zsh-syntax-highlig
 source "$HOME/.config/zsh/zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$HOME/.config/zsh/zsh-plugins/zsh-vim/vim-mode.zsh"
 source "$HOME/.config/zsh/zsh-plugins/zsh-autopair/zsh-autopair.zsh"
-source "$HOME/.config/zsh/zsh-plugins/zsh-autocomplete/zsh-autocomplete.zsh"
 source "$HOME/.config/zsh/zsh-plugins/zsh-fzf/fzf.zsh"
+source "$HOME/.config/zsh/zsh-plugins/zsh-autocomplete/zsh-autocomplete.zsh"
