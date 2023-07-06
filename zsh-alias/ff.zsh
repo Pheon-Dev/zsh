2ff5cc1cbc879efc0da73f5ded98cfeb9fba16e3
Pheon-Dev <devpheon@gmail.com>
2023-07-06 17:57:33 +0300

    [+] ripgrep

--- zsh-alias/ff.zsh
+++ zsh-alias/ff.zsh
@@ -1,9 +1,21 @@
 #!/bin/zsh zsh
 
+frg() {
+  RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+  INITIAL_QUERY="${*:-}"
+  : | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q}" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+    --delimiter : \
+    --preview 'bat --color=always {1} --highlight-line {2}' \
+    --bind 'enter:become(nvim {1} +{2})'
+}
+
 ff () {
   echo " "
   if [[ $1 == "" ]]; then
     echo "$(tput setaf 3)   [ -c, --current] from current working directory"
+    echo "$(tput setaf 3)   [ -g, --grep] search for a text"
     echo "$(tput setaf 3)   [ -h, --home   ] from $HOME"
     echo " "
     return 0
@@ -17,5 +29,8 @@ ff () {
     cd ~ && cd $(fd --type directory -H --strip-cwd-prefix --follow --exclude '.git*' | fzf)
     clear
     return 0
+  fi
+  if [[ $1 == "-g" || $1 == "--grep" ]]; then
+    frg
   fi
 }
