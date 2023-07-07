3ff215a0fae1aeb29ac01c82450aa3a74cb3adbb
Pheon-Dev <devpheon@gmail.com>
2023-07-07 16:29:29 +0300

    [~] keymaps

--- zsh-plugins/zsh-fzf/fzf.zsh
+++ zsh-plugins/zsh-fzf/fzf.zsh
@@ -16,7 +16,7 @@ export FZF_DEFAULT_OPTS="\
   --delimiter : \
   --prompt '  ' \
   --bind 'ctrl-d:change-prompt( )+reload(fd --type directory --hidden --follow --exclude \".git*\")' \
-  --bind 'ctrl-f:change-prompt( )+reload(fd --type file --hidden --follow --exclude \".git*\")'\
+  --bind 'ctrl-f:unbind(change,ctrl-f)+change-prompt( )+reload(fd --type file --hidden --follow --exclude \".git*\")'\
   --bind 'ctrl-l:execute(cd {} 2>/dev/null && nvim || nvim {})' \
   --bind 'ctrl-h:abort' \
   --color=fg:#c0caf5,bg:#21222c,hl:#bd93f9,border:#44475a \
@@ -25,7 +25,7 @@ export FZF_DEFAULT_OPTS="\
   --color=marker:#ff79c6,spinner:#f1fa8c,header:#6272a4
   "
 
-# --bind "alt-w:unbind(change,alt-w)+change-prompt(2. fzf> )+enable-search+clear-query" \
+  # --bind 'alt-w:unbind(change,alt-w)+change-prompt(2. fzf> )+enable-search+clear-query' \
 frg() {
   RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
   INITIAL_QUERY="${*:-}"
@@ -35,6 +35,8 @@ frg() {
     --delimiter : \
     --preview 'bat --color=always {1} --highlight-line {2}' \
     --bind 'enter:execute(nvim {1} +{2})' \
+    --bind 'ctrl-l:execute(nvim {1} +{2})' \
+    --bind 'ctrl-h:abort' \
     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
     --color "hl:-1:underline,hl+:-1:underline:reverse" \
 
@@ -53,7 +55,9 @@ ff () {
   if [[ $1 == "-g" || $1 == "--grep" ]]; then
     frg
   fi
-  fd --type directory -H --strip-cwd-prefix --follow --exclude '.git*' -E '.yarn' -E '.rustup' -E 'go/pkg/mod/*' -E '.cargo/registry/*' -E '.cache' -E '/home/linuxbrew' -E 'node_modules' -E 'targets' | fzf
+  if [[ $1 == "" ]]; then
+    fd --type directory -H --strip-cwd-prefix --follow --exclude '.git*' -E '.yarn' -E '.rustup' -E 'go/pkg/mod/*' -E '.cargo/registry/*' -E '.cache' -E '/home/linuxbrew' -E 'node_modules' -E 'targets' | fzf
+  fi
   clear
   return 0
 }
