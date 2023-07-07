78845e863c40fcf408d9d0faf3ac2d3d7affa012
Pheon-Dev <devpheon@gmail.com>
2023-07-07 15:51:21 +0300

    [-] ff binary

--- zsh-plugins/zsh-fzf/fzf.zsh
+++ zsh-plugins/zsh-fzf/fzf.zsh
@@ -12,14 +12,48 @@ export FZF_DEFAULT_OPTS="\
       echo {} 2> /dev/null | head -200' \
   --preview-window 'up,60%,border,+{2}+3/3,~3' \
   --ansi \
-  --header 'CTRL-H: Directories / CTRL-L: Files' \
+  --header 'CTRL-D:   Directories | CTRL-F:  Files' \
   --delimiter : \
   --prompt '  ' \
-  --bind 'ctrl-h:change-prompt( )+reload(fd --type directory --hidden --follow --exclude \".git*\")' \
-  --bind 'ctrl-l:change-prompt( )+reload(fd --type file --hidden --follow --exclude \".git*\")'\
-  --bind 'alt-i:execute(cd {} 2>/dev/null || nvim {} && nvim)' \
+  --bind 'ctrl-d:change-prompt( )+reload(fd --type directory --hidden --follow --exclude \".git*\")' \
+  --bind 'ctrl-f:change-prompt( )+reload(fd --type file --hidden --follow --exclude \".git*\")'\
+  --bind 'ctrl-l:execute(cd {} 2>/dev/null && nvim || nvim {})' \
+  --bind 'ctrl-h:abort' \
   --color=fg:#c0caf5,bg:#21222c,hl:#bd93f9,border:#44475a \
   --color=fg+:#c0caf5,bg+:#21222c,hl+:#bd93f9 \
   --color=info:#f1fa8c,prompt:#50fa7b,pointer:#ff79c6 \
   --color=marker:#ff79c6,spinner:#f1fa8c,header:#6272a4
   "
+
+# --bind "alt-w:unbind(change,alt-w)+change-prompt(2. fzf> )+enable-search+clear-query" \
+frg() {
+  RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+  INITIAL_QUERY="${*:-}"
+  : | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q}" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+    --delimiter : \
+    --preview 'bat --color=always {1} --highlight-line {2}' \
+    --bind 'enter:execute(nvim {1} +{2})' \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+    --color "hl:-1:underline,hl+:-1:underline:reverse" \
+
+  return 0
+}
+
+ff () {
+  echo " "
+  if [[ $1 == "-h" || $1 == "--help" ]]; then
+    echo "$(tput setaf 3)   [ -g, --grep   ] search for a text"
+    echo "$(tput setaf 3)   [ -h, --help   ] help"
+    echo " "
+    return 0
+  fi
+  cd
+  if [[ $1 == "-g" || $1 == "--grep" ]]; then
+    frg
+  fi
+  fd --type directory -H --strip-cwd-prefix --follow --exclude '.git*' -E '.yarn' -E '.rustup' -E 'go/pkg/mod/*' -E '.cargo/registry/*' -E '.cache' -E '/home/linuxbrew' -E 'node_modules' -E 'targets' | fzf
+  clear
+  return 0
+}
