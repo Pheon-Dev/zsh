6064f8931e47588e1058116dc8a4b5b3f7d29055
Pheon-Dev <devpheon@gmail.com>
2023-07-18 21:59:41 +0300

    [+] br [~] caps to escape

--- zsh-exports.zsh
+++ zsh-exports.zsh
@@ -1,10 +1,9 @@
 #!/bin/bash zsh
-# zmodload zsh/zprof
+# zmodload zsh/zprof # time-zsh
 
 # https://wiki.archlinux.org/title/Xorg/Keyboard_configuration
 # $ grep -E "(ctrl|caps):" /usr/share/X11/xkb/rules/base.lst
 setxkbmap -option "caps:escape"
-# setxkbmap -option "caps:ctrl_modifier"
 # setxkbmap -option "caps:ctrl_modifier"
 # setxkbmap -option "ctrl:swap_lalt_lctl"
 # xcape -e 'Control_L=Escape'
