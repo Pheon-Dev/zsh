2a99122b80c17be3972a2f9285bf917544cc3cdf
Pheon-Dev <devpheon@gmail.com>
2023-07-18 21:58:35 +0300

    [+] restore up and down keys

--- zsh-plugins/zsh-autocomplete/zsh-autocomplete.zsh
+++ zsh-plugins/zsh-autocomplete/zsh-autocomplete.zsh
@@ -20,14 +20,14 @@ unsetopt listbeep
   .autocomplete:__main__ "$@"
 } 
 
-# () {
-#    local -a prefix=( '\e'{\[,O} )
-#    local -a up=( ${^prefix}A ) down=( ${^prefix}B )
-#    local key=
-#    for key in $up[@]; do
-#       bindkey "$key" up-line-or-history
-#    done
-#    for key in $down[@]; do
-#       bindkey "$key" down-line-or-history
-#    done
-# }
+() {
+   local -a prefix=( '\e'{\[,O} )
+   local -a up=( ${^prefix}A ) down=( ${^prefix}B )
+   local key=
+   for key in $up[@]; do
+      bindkey "$key" up-line-or-history
+   done
+   for key in $down[@]; do
+      bindkey "$key" down-line-or-history
+   done
+}
