70f827c41cc051d9dc5ff85e6ad4e756e8559bdb
Pheon-Dev <devpheon@gmail.com>
2023-09-22 18:10:35 +0300

    [-] vim mode

--- zsh-exports.zsh
+++ zsh-exports.zsh
@@ -7,11 +7,11 @@ if [[ ! "$OSTYPE" =~ ^freebsd ]]; then
 
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
-export PIP_BREAK_SYSTEM_PACKAGES=1
+# export PIP_BREAK_SYSTEM_PACKAGES=1
 export PATH="$HOME/.cargo/bin:$PATH"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export BAT_CONFIG_PATH="$HOME/.config/bat/bat.conf"
